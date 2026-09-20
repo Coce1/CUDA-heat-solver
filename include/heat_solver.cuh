1ee1722529778ba83714c415a198ba2b160abc70
Can <selc911@gmaile.com>
2026-09-20 15:12:54 +0800

    n

--- include/heat_solver.cuh
+++ include/heat_solver.cuh
@@ -7,8 +7,9 @@ void solveHeatCPU(int num_points, const int* row_ptr, const int* col_idx,
                   float alpha, float dx, float dt, int steps);
 
 // GPU implementation - Global Memory (Naive)
-void solveHeatGPUNaive(float* d_u, float* d_u_tmp, int nx, 
-                        float alpha, float dx, float dt, int steps);
+void solveHeatGPUNaive(int num_points, const int* d_row_ptr, const int* d_col_idx, 
+                             const float* d_val, float* d_u, float* d_u_tmp, 
+                             float alpha, float dx, float dt, int steps);
 
 // GPU implementation - Shared Memory (Tiling with Halos)
 void solveHeatGPUSharedMatrix(int num_points, const int* d_row_ptr, const int* d_col_idx, 
